b7ae54de0432ce96060cefc02dc08761ac6cb12d
Rani Abu Sultan <raniabusultan@gmail.com>
2018-09-23 02:31:28 +0300

    todo component implementation and styling

---
+++ src/app/mock-todos.ts
@@ -0,0 +1,14 @@
+import { Todo } from './todo';
+
+export const TODOS: Todo[] = [
+  { id: 11, completed: false, description: "some text", type: "CSS" },
+  { id: 12, completed: false, description: "some text", type: "CSS" },
+  { id: 13, completed: false, description: "some text", type: "CSS" },
+  { id: 14, completed: false, description: "some text", type: "CSS" },
+  { id: 15, completed: false, description: "some text", type: "CSS" },
+  { id: 16, completed: false, description: "some text", type: "CSS" },
+  { id: 17, completed: false, description: "some text", type: "CSS" },
+  { id: 18, completed: false, description: "some text", type: "CSS" },
+  { id: 19, completed: false, description: "some text", type: "CSS" },
+  { id: 20, completed: false, description: "some text", type: "CSS" }
+];
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../todo';
+import { TODOS } from '../mock-todos';
 
 @Component({
   selector: 'app-todo',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoComponent implements OnInit {
 
+  todos = TODOS;
+  selectedTodo: Todo;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(todo: Todo): void {
+    this.selectedTodo = todo;
+    if (this.selectedTodo.completed == false) {
+      this.selectedTodo.completed = true;
+    }
+    else if (this.selectedTodo.completed == true) {
+      this.selectedTodo.completed = false;
+    }
+    console.log(this.selectedTodo);
+  }
 }
