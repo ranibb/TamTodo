b5dd50ad855bac524a353b82a3f38a1e4a24cab7
Rani Abu Sultan <raniabusultan@gmail.com>
2018-09-24 13:48:34 +0300

    Close the sidenav when any the nav-list is clicked on a Handset device.

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from "@angular/material";
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -10,11 +11,23 @@ import { map } from 'rxjs/operators';
 })
 export class MainNavComponent {
 
+  @ViewChild('drawer')
+  drawer: MatSidenav;
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
-    
+
   constructor(private breakpointObserver: BreakpointObserver) {}
-  
+
+  onClickClose() {
+    this.breakpointObserver
+      .observe([Breakpoints.Handset]) //.observe(['(max-width: 959px)'])
+      .subscribe((state: BreakpointState) => {
+        if (state.matches) {
+          this.drawer.close()
+        }
+      });
   }
+}
